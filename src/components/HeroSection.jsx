ff6b8d6270933da27563108740b1bca742ddf0ae
sunnithdev <sunnithdev@gmail.com>
2024-09-06 19:20:59 -0400

    flags

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -35,14 +35,13 @@ const HeroSection = () => {
       {/* Wrapper to center MotionBoxes */}
       <Box zIndex="0" position="relative" marginTop={['-20%','-50%',"-20%"]} display="flex" justifyContent="center" alignItems="center" minH="100vh">
         {/* Multiple Rotating Circles of Flags with Independent Speeds */}
-        {[1300, 1050, 800].map((size, idx) => (
           <MotionBox
-            key={size}
-            variants={createRotationVariants(10 + idx * 5)}  // Increase duration for each circle
+            key={1300}
+            variants={createRotationVariants(11 * 5)}  // Increase duration for each circle
             animate="rotate"
             position="absolute"
-            width={[`${size * 0.5}px`, `${size * 0.7}px`, `${size}px`]}
-            height={[`${size * 0.5}px`, `${size * 0.7}px`, `${size}px`]}
+            width={[`${1300 * 0.5}px`, `${1300 * 0.7}px`, `${1300}px`]}
+            height={[`${1300 * 0.5}px`, `${1300 * 0.7}px`, `${1300}px`]}
             border={`2px solid rgba(0, 0, 0, 0.8)`}
             transform="translate(-50%, -50%)"
             borderRadius="50%"
@@ -83,7 +82,102 @@ const HeroSection = () => {
             </Box>
             {/* Add more flags as needed */}
           </MotionBox>
-        ))}
+
+          <MotionBox
+            key={1050}
+            variants={createRotationVariants(20)}  // Increase duration for each circle
+            animate="rotate"
+            position="absolute"
+            width={[`${1050 * 0.5}px`, `${1050 * 0.7}px`, `${1050}px`]}
+            height={[`${1050 * 0.5}px`, `${1050 * 0.7}px`, `${1050}px`]}
+            border={`2px solid rgba(0, 0, 0, 0.8)`}
+            transform="translate(-50%, -50%)"
+            borderRadius="50%"
+          >
+            {/* USA Flag */}
+            <Box position="absolute" margin="-20px" backgroundColor="white" border={`2px solid rgba(0, 0, 0, 0.8)`} borderRadius="50%" padding="8px" top="0" left="50%" transform="translateX(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇺🇸
+            </Text>
+            </Box>
+            {/* Canada Flag */}
+            <Box position="absolute" margin="-20px" border={`2px solid rgba(0, 0, 0, 0.8)`} backgroundColor="white" borderRadius="50%"  padding="8px" top="50%" right="0" transform="translateY(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇨🇦
+            </Text>
+            </Box>
+            {/* France Flag */}
+            <Box position="absolute" margin="-20px" border={`2px solid rgba(0, 0, 0, 0.8)`} backgroundColor="white" borderRadius="50%" padding="8px" bottom="0" left="50%" transform="translateX(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇫🇷
+            </Text>
+            </Box>
+            {/* Germany Flag */}
+            <Box position="absolute" margin="-20px" border={`2px solid rgba(0, 0, 0, 0.8)`} backgroundColor="white" borderRadius="50%" padding="8px" top="50%" left="0" transform="translateY(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇩🇪
+            </Text>
+            </Box>
+            <Box position="absolute" margin="-20px" border={`2px solid rgba(0, 0, 0, 0.8)`} backgroundColor="white" borderRadius="50%" padding="8px" top="50%" left="0" transform="translateY(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇮🇳
+            </Text>
+            </Box>
+            <Box position="absolute" margin="-20px" border={`2px solid rgba(0, 0, 0, 0.8)`} backgroundColor="white" borderRadius="50%" padding="8px" top="50%" left="0" transform="translateY(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇲🇽
+            </Text>
+            </Box>
+            {/* Add more flags as needed */}
+          </MotionBox>
+
+          <MotionBox
+            key={800}
+            variants={createRotationVariants(10)}  // Increase duration for each circle
+            animate="rotate"
+            position="absolute"
+            width={[`${800 * 0.5}px`, `${800 * 0.7}px`, `${800}px`]}
+            height={[`${800 * 0.5}px`, `${800 * 0.7}px`, `${800}px`]}
+            border={`2px solid rgba(0, 0, 0, 0.8)`}
+            transform="translate(-50%, -50%)"
+            borderRadius="50%"
+          >
+            {/* USA Flag */}
+            <Box position="absolute" margin="-20px" backgroundColor="white" border={`2px solid rgba(0, 0, 0, 0.8)`} borderRadius="50%" padding="8px" top="0" left="50%" transform="translateX(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇺🇸
+            </Text>
+            </Box>
+            {/* Canada Flag */}
+            <Box position="absolute" margin="-20px" border={`2px solid rgba(0, 0, 0, 0.8)`} backgroundColor="white" borderRadius="50%"  padding="8px" top="50%" right="0" transform="translateY(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇨🇦
+            </Text>
+            </Box>
+            {/* France Flag */}
+            <Box position="absolute" margin="-20px" border={`2px solid rgba(0, 0, 0, 0.8)`} backgroundColor="white" borderRadius="50%" padding="8px" bottom="0" left="50%" transform="translateX(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇫🇷
+            </Text>
+            </Box>
+            {/* Germany Flag */}
+            <Box position="absolute" margin="-20px" border={`2px solid rgba(0, 0, 0, 0.8)`} backgroundColor="white" borderRadius="50%" padding="8px" top="50%" left="0" transform="translateY(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇩🇪
+            </Text>
+            </Box>
+            <Box position="absolute" margin="-20px" border={`2px solid rgba(0, 0, 0, 0.8)`} backgroundColor="white" borderRadius="50%" padding="8px" top="50%" left="0" transform="translateY(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇮🇳
+            </Text>
+            </Box>
+            <Box position="absolute" margin="-20px" border={`2px solid rgba(0, 0, 0, 0.8)`} backgroundColor="white" borderRadius="50%" padding="8px" top="50%" left="0" transform="translateY(-50%)">
+            <Text fontSize="2xl" fontWeight="bold" lineHeight="shorter">
+            🇲🇽
+            </Text>
+            </Box>
+            {/* Add more flags as needed */}
+          </MotionBox>
 
         {/* Add the SVG at the center */}
         <Box zIndex="10" position="absolute" top={['30%','35%',"40%"]}>
